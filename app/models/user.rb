ecf90e774b3d059c687f3be78e2435a42398baef
Ivan Heneghan <ivanheneghan@gmail.com>
2016-05-16 14:58:07 +0000

    25

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,13 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def enrolled_in?(course)
+    enrolled_courses = []
+    enrollments.each do |enrollment|
+      enrolled_courses << enrollment.course
+    end
+
+    return enrolled_courses.include?(course)
+  end
 end
