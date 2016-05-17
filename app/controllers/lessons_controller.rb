bc695e10b30817002c4762f70e1ce6240d22563a
Ivan Heneghan <ivanheneghan@gmail.com>
2016-05-17 10:28:08 +0000

    25

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -8,9 +8,8 @@ class LessonsController < ApplicationController
   private
 
   def require_authorized_for_current_lesson
-    the_course = current_lesson.section.course
-    if !current_lesson.section.course.user.enrolled_in?(the_course)
-      redirect_to course_path(the_course), alert: 'Please enroll to access lessons'
+    if !current_lesson.section.course.user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: 'Please enroll to access lessons'
     end
   end
   
